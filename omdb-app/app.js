2a4b5f8d477358a1d5c911810ccb0f257d3aa3b2
Tania Diane Dodson <taniad0916@MacBook.local>
2020-02-22 16:02:49 -0500

    random quote function

--- omdb-app/app.js
+++ omdb-app/app.js
@@ -34,3 +34,25 @@ $(() => {
 
   })
 })
+
+const randNum = () => {
+  return Math.floor(Math.random() * quotes.length)
+}
+
+const quotes = ['Come out to the coast, we’ll get together, have a few laughs…',
+                'Just a fly in the ointment, Hans. A monkey in the wrench.',
+                'Welcome to the party, Pal!',
+                'Yippie Ki Yay, M*th*rf*ck*r!',
+                'Nine million terrorists in the world and I gotta kill one with feet smaller than my sister.',
+                'No f*cking sh*t lady, does it sound like I\'m ordering a pizza?',
+                'Happy trails, Hans!']
+
+const randQuote = () => {
+  return quotes[randNum()]
+}
+
+// randNum()
+// console.log(randNum())
+
+randQuote()
+console.log(quotes[randNum()])
